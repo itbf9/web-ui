63d440cb1f95c7b69548181a3b2dba6a3b67da40
itbf9 <itbf@proton.me>
2023-10-19 10:27:55 +0200

    code format + comments on prop resolver

--- src/app/core/_services/property-resolver.ts
+++ src/app/core/_services/property-resolver.ts
@@ -1,9 +1,36 @@
+/**
+ * Utility class for resolving properties in a nested object by providing a property path.
+ *
+ * @example
+ * To access a nested property within an object:
+ *
+ * const obj = {
+ *   person: {
+ *     name: 'John',
+ *     address: {
+ *       city: 'New York'
+ *     }
+ *   }
+ * };
+ *
+ * const city = PropertyResolver.resolve('person.address.city', obj); // Returns 'New York'
+ *
+ * @class PropertyResolver
+ */
 export class PropertyResolver {
-     static resolve(path: string, obj: any) {
-      return path.split('.').reduce((prev, curr) => {
-          return (prev ? prev[curr] : undefined);
-      }, obj || self);
-    }
+  /**
+   * Resolves a property within a nested object by providing a property path.
+   *
+   * @param {string} path - The property path to resolve (e.g., 'person.address.city').
+   * @param {any} obj - The object in which to resolve the property.
+   * @returns {any} The resolved property value or `undefined` if the path is not found.
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  static resolve(path: string, obj: any) {
+    return path.split('.').reduce((prev, curr) => {
+      return (prev ? prev[curr] : undefined);
+    }, obj || self);
+  }
 }
 
 
