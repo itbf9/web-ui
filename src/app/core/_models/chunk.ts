58e0e97df2ab7c98d94b22798b2993a0325c032f
itbf9 <itbf@proton.me>
2023-10-19 10:01:35 +0200

    Add chunk interface

--- src/app/core/_models/chunk.ts
+++ src/app/core/_models/chunk.ts
@@ -1,16 +1,38 @@
-export interface BaseChunk {
-  chunkId: number,
-  taskId: number,
-  format: string,
-  skip: string,
-  length: number,
-  agentId: number,
-  dispatchTime: number,
-  solveTime: number,
-  checkpoINT: number,
-  progress: number,
-  state: number,
-  cracked: number,
-  speed: number,
-  }
+import { Agent } from "./agents"
+import { Task } from "./task"
 
+export interface BaseChunk {
+  chunkId: number
+  taskId: number
+  format: string
+  skip: string
+  length: number
+  agentId: number
+  dispatchTime: number
+  solveTime: number
+  checkpoint: number
+  progress: number
+  state: number
+  cracked: number
+  speed: number
+}
+
+export interface Chunk {
+  _id: number
+  _self: string
+  chunkId: number
+  taskId: number
+  task?: Task
+  format: string
+  skip: number
+  length: number
+  agentId: number
+  agent?: Agent
+  dispatchTime: number
+  solveTime: number
+  checkpoint: number
+  progress: number
+  state: number
+  cracked: number
+  speed: number
+}
