a3a1e69ec2e42a0c064608ce8385c0ad958ac263
itbf9 <itbf@proton.me>
2023-10-19 10:00:50 +0200

    Add agent + agentstats interfaces

--- src/app/core/_models/agents.ts
+++ src/app/core/_models/agents.ts
@@ -1,25 +1,49 @@
+import { AccessGroup } from "./access-group"
+import { User } from "./user.model"
+
+export interface AgentStats {
+  _id: number
+  _self: string
+  agentStatId: number
+  agentId: number
+  statType: number
+  time: number
+  value: number[]
+}
+
+export interface Agent {
+  agentId: number
+  agentName: string
+  uid: string
+  os: number
+  devices: string
+  cmdPars: string
+  ignoreErrors: number
+  isActive: boolean
+  isTrusted: boolean
+  token: string
+  lastAct: string
+  lastTime: number
+  lastIp: string
+  userId: number
+  user?: User
+  cpuOnly: number
+  clientSignature: string
+  agentstats?: AgentStats[]
+  accessGroups?: AccessGroup[]
+  _id?: number
+  _self?: string
+}
+
+
+
+
+
 export interface IAgents {
-    _expandable?: string;
-    startAt: number;
-    maxResults: number;
-    total: number;
-    isLast: string;
-    values: [{
-      agentId: number;
-      agentName: string;
-      uid: string;
-      os: number;
-      devices: string;
-      cmdPars: string;
-      ignoreErrors: string;
-      isActive: string;
-      isTrusted: string;
-      token: string;
-      lastAct: string;
-      lastTime: number;
-      lastIp: string;
-      userId: number;
-      cpuOnly: number;
-      clientSignature: string;
-    }]
+  _expandable?: string
+  startAt: number
+  maxResults: number
+  total: number
+  isLast: string
+  values: Agent[]
 }
