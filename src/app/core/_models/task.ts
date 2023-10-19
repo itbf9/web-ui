b6171fb311b10fe05fef13726273fe5da86cb4cf
itbf9 <itbf@proton.me>
2023-10-19 10:02:18 +0200

    Add task interface

--- src/app/core/_models/task.ts
+++ src/app/core/_models/task.ts
@@ -1,11 +1,40 @@
 // import { Hashlist } from "./hashlist";
 
 export interface NormalTask {
-    id: number;
-    name: string;
-    priority: number;
-    maxAgents: number;
+  id: number;
+  name: string;
+  priority: number;
+  maxAgents: number;
 
-    hashlistId: number;
-    // hashlist: Hashlist;
+  hashlistId: number;
+  // hashlist: Hashlist;
+}
+
+export interface Task {
+  _id: number
+  _self: string
+  attackCmd: string
+  chunkSize: number
+  chunkTime: number
+  color?: string
+  crackerBinaryId: number
+  crackerBinaryTypeId: number
+  forcePipe: boolean
+  isArchived: boolean
+  isCpuTask: boolean
+  isSmall: boolean
+  keyspace: number
+  keyspaceProgress: number
+  maxAgents: number
+  notes: string
+  preprocessorCommand: number
+  preprocessorId: number
+  priority: number
+  skipKeyspace: number
+  staticChunks: number
+  statusTimer: number
+  taskId: number
+  taskName: string
+  taskWrapperId: number
+  useNewBench: boolean
 }
