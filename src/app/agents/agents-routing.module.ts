e5db2cd638e8b7b694773d2d1c464cec2dfb2612
itbf9 <itbf@proton.me>
2023-10-19 10:43:23 +0200

    Add agent resolver

--- src/app/agents/agents-routing.module.ts
+++ src/app/agents/agents-routing.module.ts
@@ -2,12 +2,11 @@ import { CheckPerm } from "../core/_guards/permission.guard";
 import { IsAuth } from "../core/_guards/auth.guard";
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from '@angular/router';
-
 import { AgentStatusComponent } from "./agent-status/agent-status.component";
-import { PendingChangesGuard } from "../core/_guards/pendingchanges.guard";
 import { ShowAgentsComponent } from "./show-agents/show-agents.component";
 import { EditAgentComponent } from "./edit-agent/edit-agent.component";
 import { NewAgentComponent } from "./new-agent/new-agent.component";
+import { agentResolver } from "../core/_resolvers/agent.resolver";
 
 const routes: Routes = [
   {
@@ -16,38 +15,42 @@ const routes: Routes = [
       {
         path: 'agent-status', component: AgentStatusComponent,
         data: {
-            kind: 'agent-status',
-            breadcrumb: 'Agent Status',
-            permission: 'Agent' //ToDo this one has Agent read and Agent Stats read
+          kind: 'agent-status',
+          breadcrumb: 'Agent Status',
+          permission: 'Agent' //ToDo this one has Agent read and Agent Stats read
         },
-        canActivate: [IsAuth,CheckPerm]
+        canActivate: [IsAuth, CheckPerm]
       },
       {
         path: 'new-agent', component: NewAgentComponent,
         data: {
-            kind: 'new-agent',
-            breadcrumb: 'New Agent',
-            permission: 'Agent'
+          kind: 'new-agent',
+          breadcrumb: 'New Agent',
+          permission: 'Agent'
         },
-        canActivate: [IsAuth,CheckPerm]},
+        canActivate: [IsAuth, CheckPerm]
+      },
       {
         path: 'show-agents', component: ShowAgentsComponent,
         data: {
-            kind: 'show-agents',
-            breadcrumb: 'Show Agent',
-            permission: 'Agent'
+          kind: 'show-agents',
+          breadcrumb: 'Show Agent',
+          permission: 'Agent'
         },
-        canActivate: [IsAuth,CheckPerm]},
+        canActivate: [IsAuth, CheckPerm]
+      },
       {
         path: 'show-agents/:id/edit', component: EditAgentComponent,
         data: {
-            kind: 'edit-agent',
-            breadcrumb: 'Edit Agent',
-            permission: 'Agent'
+          kind: 'edit-agent',
+          breadcrumb: 'Edit Agent',
+          permission: 'Agent'
         },
-        canActivate: [IsAuth,CheckPerm]},
-      ]
-    },
+        resolve: { agent: agentResolver },
+        canActivate: [IsAuth, CheckPerm]
+      },
+    ]
+  },
 ];
 
 @NgModule({
@@ -55,4 +58,4 @@ const routes: Routes = [
   exports: [RouterModule]
 
 })
-export class AgentsRoutingModule {}
+export class AgentsRoutingModule { }
