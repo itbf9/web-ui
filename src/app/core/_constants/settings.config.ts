d8de41703521757dd69ec06aacf1ccb5a94a77b7
itbf9 <itbf@proton.me>
2023-10-30 16:57:33 +0100

    Add iso date

--- src/app/core/_constants/settings.config.ts
+++ src/app/core/_constants/settings.config.ts
@@ -2,30 +2,41 @@
  * Date formats, used in general settings and when app is initialized
 **/
 
-export const dateFormat = [
-  {format:'d/M/yy', description:'d/M/yy (ie. 6/7/23 )'},
-  {format:'dd/MM/yyyy h:mm:ss', description:'dd/MM/yyyy h:mm:ss (ie. 06/07/2023, 9:03 AM)'},
-  {format:'d MMM, y h:mm:ss a', description:'dd/MM/yyyy h:mm:ss (ie. 06 Jul, 2023 9:03:01 AM)'},
-  {format:'M/d/yy', description:'M/d/yy (ie. 7/6/23)'},
-  {format:'M/d/yy, h:mm a', description:'M/d/yy, h:mm a (ie. 7/6/23, 9:03 AM)'},
-  {format:'MMM d, y, h:mm:ss a', description:'MMM d, y, h:mm:ss a (ie. Jul 06, 2023, 9:03:01 AM)'},
-  {format:'yy/M/d', description:'yy/M/d (ie. 23/7/6 )'},
-  {format:'yyyy/M/d, h:mm:ss a', description:'yyyy/M/d (ie. 2023/7/6, 9:03:01 AM )'},
-  {format:'yyyy/MM/dd h:mm:ss', description:'yyyy/MM/dd h:mm:ss (ie. 2023/07/06, 9:03 AM)'},
+export interface Setting {
+  value: string
+  description: string
+}
+
+export const dateFormats: Setting[] = [
+  { value: 'd/M/yy', description: 'd/M/yy (ie. 6/7/23 )' },
+  { value: 'dd/MM/yyyy h:mm:ss', description: 'dd/MM/yyyy h:mm:ss (ie. 06/07/2023, 9:03 AM)' },
+  { value: 'd MMM, y h:mm:ss a', description: 'dd/MM/yyyy h:mm:ss (ie. 06 Jul, 2023 9:03:01 AM)' },
+  { value: 'M/d/yy', description: 'M/d/yy (ie. 7/6/23)' },
+  { value: 'M/d/yy, h:mm a', description: 'M/d/yy, h:mm a (ie. 7/6/23, 9:03 AM)' },
+  { value: 'MMM d, y, h:mm:ss a', description: 'MMM d, y, h:mm:ss a (ie. Jul 06, 2023, 9:03:01 AM)' },
+  { value: 'yy/M/d', description: 'yy/M/d (ie. 23/7/6 )' },
+  { value: 'yyyy/M/d, h:mm:ss a', description: 'yyyy/M/d (ie. 2023/7/6, 9:03:01 AM )' },
+  { value: 'yyyy/MM/dd h:mm:ss', description: 'yyyy/MM/dd h:mm:ss (ie. 2023/07/06, 9:03 AM)' },
+  { value: 'yyyy-MM-dd h:mm:ss', description: 'yyyy-MM-dd h:mm:ss (ie. 2023-07-06, 09:03)' },
 ];
 
+
+export const layouts: Setting[] = [
+  { value: 'fixed', description: 'Fixed width layout' },
+  { value: 'full', description: 'Full screen layout' },
+]
 
 /**
  * Logs, used in general settings
 **/
 
 export const serverlog = [
-  {id:0, value: 'TRACE'},
-  {id:10, value: 'DEBUG'},
-  {id:20, value: 'INFO'},
-  {id:30, value: 'WARNING'},
-  {id:40, value: 'ERROR'},
-  {id:50, value: 'FATAL'}
+  { id: 0, value: 'TRACE' },
+  { id: 10, value: 'DEBUG' },
+  { id: 20, value: 'INFO' },
+  { id: 30, value: 'WARNING' },
+  { id: 40, value: 'ERROR' },
+  { id: 50, value: 'FATAL' }
 ];
 
 /**
@@ -33,10 +44,10 @@ export const serverlog = [
 **/
 
 export const proxytype = [
-  {value:'HTTP'},
-  {value:'HTTPS'},
-  {value:'SOCKS4'},
-  {value:'SOCKS5'}
+  { value: 'HTTP' },
+  { value: 'HTTPS' },
+  { value: 'SOCKS4' },
+  { value: 'SOCKS5' }
 ];
 
 /**
