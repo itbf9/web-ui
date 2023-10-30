09eab791754df04343178186cfa9bb26f67d02a6
itbf9 <itbf@proton.me>
2023-10-30 16:41:09 +0100

    Add config interfaces and default values

--- src/app/core/_models/config-ui.model.ts
+++ src/app/core/_models/config-ui.model.ts
@@ -1,3 +1,7 @@
+import { AgentsTableColumnLabel } from "../_components/agents-table/agents-table.constants"
+import { ChunksTableColumnLabel } from "../_components/chunks-table/chunks-table.constants"
+import { TaskTableColumnLabel } from "../_components/tasks-table/tasks-table.constants"
+
 export class IStorage {
   constructor(
     private _timefmt: string,
@@ -8,14 +12,60 @@ export class IStorage {
     private _statimer: string,
     private _timestamp: number,
     private _expiresin: number,
-  ) {}
+  ) { }
 
 }
 
-export class UIGConfig {
-  constructor(
+export type Layout = 'full' | 'fixed'
+export type Theme = 'light' | 'dark'
 
-  ) {}
-
+export interface TableSettings {
+  [key: string]: string[]
 }
 
+export interface UIConfig {
+  layout: Layout
+  theme: Theme
+  tableSettings: TableSettings,
+  timefmt: string,
+}
+
+export const uiConfigDefault: UIConfig = {
+  layout: 'fixed',
+  theme: 'light',
+  timefmt: 'dd/MM/yyyy h:mm:ss',
+  tableSettings: {
+    agentTable: [
+      AgentsTableColumnLabel.ID,
+      AgentsTableColumnLabel.STATUS,
+      AgentsTableColumnLabel.NAME,
+      AgentsTableColumnLabel.USER,
+      AgentsTableColumnLabel.CLIENT,
+      AgentsTableColumnLabel.GPUS_CPUS,
+      AgentsTableColumnLabel.LAST_ACTIVITY,
+      AgentsTableColumnLabel.ACCESS_GROUP
+    ],
+    chunkTable: [
+      ChunksTableColumnLabel.ID,
+      ChunksTableColumnLabel.PROGRESS,
+      ChunksTableColumnLabel.TASK,
+      ChunksTableColumnLabel.AGENT,
+      ChunksTableColumnLabel.DISPATCH_TIME,
+      ChunksTableColumnLabel.LAST_ACTIVITY,
+      ChunksTableColumnLabel.TIME_SPENT,
+      ChunksTableColumnLabel.STATE,
+      ChunksTableColumnLabel.CRACKED,
+    ],
+    taskTable: [
+      TaskTableColumnLabel.ID,
+      TaskTableColumnLabel.NAME,
+      TaskTableColumnLabel.STATUS,
+      TaskTableColumnLabel.ATTACK_CMD,
+      TaskTableColumnLabel.HASHLISTS,
+      TaskTableColumnLabel.CRACKED,
+      TaskTableColumnLabel.AGENTS,
+      TaskTableColumnLabel.SPEED,
+      TaskTableColumnLabel.PRIORITY,
+    ]
+  }
+}
