4490cf872263b5e915172c8f14190cb0700c51ca
itbf9 <itbf@proton.me>
2023-10-30 16:59:23 +0100

    Set change detection strategy to onpush

--- src/app/tasks/chunks/chunks.component.ts
+++ src/app/tasks/chunks/chunks.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AutoTitleService } from 'src/app/core/_services/shared/autotitle.service';
 
 @Component({
   selector: 'app-chunks',
-  templateUrl: './chunks.component.html'
+  templateUrl: './chunks.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ChunksComponent {
   constructor(private titleService: AutoTitleService) {
